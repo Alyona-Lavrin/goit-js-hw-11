ea3d9d6da52999988365a4bb0f084b3f364d98e5
Alona <polejcukalona@gmail.com>
2023-08-16 23:48:04 +0300

    sadsada

--- src/js/api/cat-api.js
+++ src/js/api/cat-api.js
@@ -1,16 +1,12 @@
 import axios from 'axios';
 import { MarkUpInterface } from '../service-markup/service-markup';
+import { API_KEY, URL_SELECT, URL_BREED_ID } from '../consts'
 
-const API_KEY =
-  'live_MiVfphVZ48xXVhbZoo5AxYZz22Ynr1MoZ2ilZG95xdjhSBJlnIUsXmt8w5XeGtL4';
-const URL_SELECT = 'https://api.thecatapi.com/v1/breeds';
-const URL_BREED_ID = 'https://api.thecatapi.com/v1/images/search';
-
-const theCatApi = axios.create();
-theCatApi.defaults.headers.common['x-api-key'] = API_KEY;
+const catApi = axios.create();
+catApi.defaults.headers.common['x-api-key'] = API_KEY;
 
 function fetchBreeds() {
-  return theCatApi
+  return catApi
     .get(URL_SELECT)
     .then(response => response.data)
     .catch(() => MarkUpInterface.showError('select'));
@@ -18,7 +14,7 @@ function fetchBreeds() {
 
 function fetchCatByBreed(evt) {
   const { value } = evt.target;
-  return theCatApi
+  return catApi
     .get(URL_BREED_ID, {
       params: {
         breed_ids: value,
---
+++ src/js/consts.js
@@ -0,0 +1,4 @@
+export const API_KEY =
+'live_MiVfphVZ48xXVhbZoo5AxYZz22Ynr1MoZ2ilZG95xdjhSBJlnIUsXmt8w5XeGtL4';
+export const URL_SELECT = 'https://api.thecatapi.com/v1/breeds';
+export const URL_BREED_ID = 'https://api.thecatapi.com/v1/images/search';
