3e4aad82bde01262319ece64ccaf1caee8204079
Alona <polejcukalona@gmail.com>
2023-08-23 23:22:25 +0300

    asdasd

--- src/index.js
+++ src/index.js
@@ -1,92 +1,124 @@
-import './sass/main.scss';
-import { fetchImages } from './js/fetch-images';
-import { renderGallery } from './js/render-gallery';
-import { onScroll, onToTopBtn } from './js/scroll';
-import Notiflix from 'notiflix';
+import NewsApiService from './api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css'; // Додатковий імпорт стилів
+import scrollToTop from './scroll-to-top';
 
-import 'simplelightbox/dist/simple-lightbox.min.css';
+const newsApiService = new NewsApiService(); //Класс з методами
 
-const searchForm = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.btn-load-more');
-let query = '';
-let page = 1;
-let simpleLightBox;
-const perPage = 40;
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const btnLoadMoreEl = document.querySelector('.load-more');
 
-searchForm.addEventListener('submit', onSearchForm);
-loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+const perPage = newsApiService.perPage;
+let score = perPage;
 
-onScroll();
-onToTopBtn();
+formEl.addEventListener('submit', handlerSubmitForm);
 
-function onSearchForm(e) {
-  e.preventDefault();
-  window.scrollTo({ top: 0 });
-  page = 1;
-  query = e.currentTarget.searchQuery.value.trim();
-  gallery.innerHTML = '';
-  loadMoreBtn.classList.add('is-hidden');
+async function handlerSubmitForm(ev) {
+  ev.preventDefault();
+  galleryEl.innerHTML = '';
+  hiddenEl(btnLoadMoreEl);
+  score = perPage;
 
-  if (query === '') {
-    alertNoEmptySearch();
-    return;
+  newsApiService.query = ev.currentTarget.searchQuery.value;
+  newsApiService.resetPageToDefault();
+
+  try {
+    const data = await newsApiService.fetchSearch();
+    if (parseInt(data.totalHits) <= 0) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    createRenderMarkup(data.hits);
+    if (data.totalHits > perPage) showEl(btnLoadMoreEl);
+  } catch (error) {
+    Notify.failure(`${error}`);
+    console.log(error);
   }
-
-  fetchImages(query, page, perPage)
-    .then(({ data }) => {
-      if (data.totalHits === 0) {
-        alertNoImagesFound();
-      } else {
-        renderGallery(data.hits);
-        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
-        alertImagesFound(data);
-
-        if (data.totalHits > perPage) {
-          loadMoreBtn.classList.remove('is-hidden');
-        }
-      }
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      searchForm.reset();
-    });
+  formEl.reset();
 }
 
-function onLoadMoreBtn() {
-  page += 1;
-  simpleLightBox.destroy();
-
-  fetchImages(query, page, perPage)
-    .then(({ data }) => {
-      renderGallery(data.hits);
-      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
-
-      const totalPages = Math.ceil(data.totalHits / perPage);
-
-      if (page > totalPages) {
-        loadMoreBtn.classList.add('is-hidden');
-        alertEndOfSearch();
-      }
-    })
-    .catch(error => console.log(error));
+function createRenderMarkup(arr) {
+  createMarkup(arr);
+  lightbox.refresh();
 }
 
-function alertImagesFound(data) {
-  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-}
-
-function alertNoEmptySearch() {
-  Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
-}
-
-function alertNoImagesFound() {
-  Notiflix.Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.',
+function createMarkup(arr) {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    arr
+      .map(
+        el =>
+          `<div class="wrapper-image">
+          <a class="gallery__link" href="${el.largeImageURL}">
+            <div class="gallery__thumb">
+              <img class="gallery__image" src="${el.webformatURL}" alt="${el.tags}" loading="lazy"/>
+            </div>
+          </a>
+          <div class="info">
+            <p class="info-item"><b>Likes  </b>${el.likes}</p>
+            <p class="info-item"><b>Views </b>${el.views} </p>
+            <p class="info-item"><b>Comments </b>${el.comments} </p>
+            <p class="info-item"><b>Downloads</b> ${el.downloads}</p>
+          </div>
+        </div>`
+      )
+      .join(' ')
   );
 }
 
-function alertEndOfSearch() {
-  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.93,
+  swipeTolerance: 400,
+  // showCounter: false,
+});
+
+btnLoadMoreEl.addEventListener('click', fetchLoadMore);
+
+async function fetchLoadMore() {
+  newsApiService.incrementPage();
+  const data = await newsApiService.fetchSearch();
+  try {
+    createRenderMarkup(data.hits);
+    score += data.hits.length;
+
+    // console.log(  'data.hits: ',
+    //   data.hits.length, 'page: ', newsApiService.page,
+    //   'total: ',  score
+    // );
+    smoothPageScrolling();
+
+    if (score >= data.totalHits) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      return hiddenEl(btnLoadMoreEl);
+    }
+  } catch (error) {
+    Notify.failure(`${error}`);
+    console.log(error);
+  }
+}
+
+function hiddenEl(element) {
+  element.classList.add('is-hidden');
+}
+
+function showEl(element) {
+  element.classList.remove('is-hidden');
+}
+
+//Плавна прокрутка після завантаження нових зображень
+function smoothPageScrolling() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+  });
 }
